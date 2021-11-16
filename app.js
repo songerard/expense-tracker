615ddc70434d3eb3bcf6eb350d389f665df1390e
songerard <songerard@gmail.com>
2021-11-16 13:34:23 +0800

    feat: add login and register page

--- app.js
+++ app.js
@@ -5,12 +5,32 @@ const exphbs = require('express-handlebars')
 const app = express()
 
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
-app.set('view engine', 'handlebars')
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
+app.set('view engine', 'hbs')
 
 
 app.get('/', (req, res) => {
   res.send('app.js')
+})
+
+app.get('/users/login', (req, res) => {
+  res.render('login')
+})
+
+app.post('/users/login', (req, res) => {
+  res.send('login')
+})
+
+app.get('/users/register', (req, res) => {
+  res.render('register')
+})
+
+app.post('/users/register', (req, res) => {
+  res.send('register')
+})
+
+app.get('/users/logout', (req, res) => {
+  res.send('logout')
 })
 
 const PORT = 3000
